4cde0861e9822485e78104895bf22c3f1eb9e464
David M Vermillion <social@davidmvermillion.com>
2020-12-13 15:37:34 -0500

    Labeled Election Day
    Added label for datapoint in plot

--- ChartsforMedium.R
+++ ChartsforMedium.R
@@ -9,7 +9,16 @@ raw <- as_tibble(raw)
 calls <- raw
 calls$InboundCalls <- calls$InboundCalls %>% replace_na(0)
 
+
 # Plot Inbound Calls
 ggplot(calls, aes(x=Day, y=InboundCalls)) +
+  theme_classic() +
   geom_bar(stat="identity", fill="steelblue") +
-  theme_classic()
+  geom_label(label = "Election Day", x = 18.45, y = 220,
+             label.padding = unit(0.25, "lines"), # Rectangle size around label
+             label.r = unit(0, "lines"),
+             label.size = 0.35,
+             color = "white",
+             fill="grey",
+             inherit.aes = TRUE)
+  
